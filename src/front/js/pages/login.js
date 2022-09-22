ebe7ee54bb8e38cf29abd47cb5930f704ea155c1
stevencarle93 <stevencarle@hotmail.com>
2022-09-22 03:35:44 +0000

    Changes Is3 into Is5 Sept 21 2022 @ 11:35 PM EST

---
+++ src/front/js/pages/login.js
@@ -0,0 +1,80 @@
+import React, { useContext, useState } from "react";
+import { Context } from "../store/appContext";
+import { Link } from "react-router-dom";
+import "../../styles/home.css";
+
+export const Login = () => {
+  const { store, actions } = useContext(Context);
+  const [color, changeColor] = useState("#F2EBE1");
+	document.body.style.backgroundColor = color;
+  /*
+  const login = async (e) => {
+    e.preventDefault();
+    const data = new FormData(e.target);
+    let email = data.get("email");
+    let password = data.get("password");
+    let loginData = {
+      email: email,
+      password: password,
+    };
+    let response = await actions.login("login", loginData, 'POST');
+    if (response.ok) {
+      response = await response.json()
+      
+      //redirigir a la landingPage
+    
+    } else {
+      response = await response.json();
+      console.log(response.message)
+      alert("Usuario o constraseña inválidos");
+      return;
+    }
+  };
+  */
+  return (
+    <>
+      <div
+        className="container d-flex justify-content-center align-items-center letraKurius"
+        style={{ height: "40vw" }}
+      >
+        <div className="card text-center border-0" style={{ width: "50%" }}>
+          <div className="card-header cabezoteRegistro" style={{ width: "100%" }}>
+            <h3>Login</h3>
+          </div>
+          <div className="card-body cajatextoRegistro" style={{ width: "100%" }}>
+            <form onSubmit="{(e) => login(e)}">
+              <div className=" d-flex flex-column bd-highlight mb-3">
+                <div className="d-flex mb-3">
+                  <h5 className="col-4">Email</h5>
+                  <input
+                    className="col-6"
+                    name="email"
+                    placeholder="Escriba aquí su email"
+                    type="email"
+                  />
+                </div>
+                <div className="d-flex ">
+                  <h5 className="col-4">Contraseña</h5>
+                  <input
+                    className="col-6"
+                    name="password"
+                    placeholder="Escriba aquí su clave"
+                    type="password"
+                  />
+                </div>
+              </div>
+              <button className="btn btn-success" type="submit">
+                Acceder
+              </button>
+            </form>
+            <div className="mt-3">
+              <Link to={"/restorepassword"} onClick={() => changeColor("#F2EBE1")}>
+                <span>Olvidé mi contraseña</span>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
