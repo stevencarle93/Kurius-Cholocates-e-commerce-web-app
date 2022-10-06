64f8914342e70d0553fa251cb15059a7136b1fab
stevencarle93 <stevencarle@hotmail.com>
2022-10-06 02:00:54 +0000

    Details Format Done Oct 5 2022 @ 10 PM EST

--- src/front/js/component/ProductsDetails5.js
+++ src/front/js/component/ProductsDetails5.js
@@ -5,20 +5,30 @@ import { Link } from "react-router-dom";
 
 export const ProductsDetails5 = () => {
   return (
-    <div className="card mb-3">
+    <div className="card mb-3 d-flex justify-content-center">
       <div className="row no-gutters">
         <div className="col-md-4">
           <img src={Cobertura_Blanco_35} className="card-img" alt="..."></img>
         </div>
-        <div className="col-md-8">
+        <div className="col">
           <div className="card-body">
-            <h5 className="card-title">Cobertura de Blanco 35%</h5>
-            <p className="card-text">
-              Curva de Temperatura Fusión: 40° - 45° (max)
-Descenso: 25° - 27°
-Cristalización: 28° - 30°
-            </p>
-            <p>Usos: tabletas, bombones, trufas, pasteleria, postres</p>
+            <h5 className="card-title col-md-12 text-center">Cobertura de Blanco 35%</h5> <br></br>
+            <div className="row">
+              <br></br>
+              <div className="col-md-4">
+                <h6>Curva de Temperatura</h6> Fusión: 40° - 45° (max) <br></br>
+                Descenso: 25° - 27° <br></br>Cristalización: 28° - 30°
+                <p>
+                <br></br> <h6>Usos</h6> tabletas, bombones, trufas, pasteleria, postres
+                </p>
+              </div>
+              <div className="col-md-4 vl">
+                <h6>Perfil organoléptico</h6> <br></br> buen brillo, buen nivel
+                de snap, sabor a leche, cremoso, ingrediente principal manteca
+                de cacao, dulzor equilibrado. <br></br> <br></br>{" "}
+                <h6>Fluidez</h6> 5/5
+              </div>
+            </div>
           </div>
         </div>
       </div>
