29b0f2e29ecc7f2b2b4a06e87e696f61990c38dc
stevencarle93 <stevencarle@hotmail.com>
2022-10-07 03:12:50 +0000

    Detail Views Done Oct 6 @ 11:12 PM EST

--- src/front/js/component/ProductsDetails9.js
+++ src/front/js/component/ProductsDetails9.js
@@ -10,15 +10,23 @@ export const ProductsDetails9 = () => {
         <div className="col-md-4">
           <img src={Cobertura_Negro_70} className="card-img" alt="..."></img>
         </div>
-        <div className="col-md-8">
+        <div className="col">
           <div className="card-body">
-            <h5 className="card-title">Cobertura de Chocolate Semi Amargo 70%</h5>
-            <p className="card-text">
-              Curva de Temperatura Fusión: 50° - 55°
-Descenso: 28° - 29°
-Cristalización: 30° - 31°
-            </p>
-            <p>Usos: tabletas, bombones, trufas, pasteleria, postres, heladeria, galleteria, ganache</p>
+            <h5 className="card-title col-md-12 text-center">Cobertura de Chocolate Semi Amargo 70%</h5> <br></br>
+            <div className="row">
+              <br></br>
+              <div className="col-md-4">
+                <h6>Curva de Temperatura</h6> Fusión: 50° - 55° <br></br>
+                Descenso: 28° - 29° <br></br>Cristalización: 30° - 31°
+                <p>
+                <br></br> <h6>Usos</h6> tabletas, bombones, trufas, pasteleria, postres, heladeria, galleteria, ganache
+                </p>
+              </div>
+              <div className="col-md-4 vl">
+                <h6>Perfil organoléptico</h6> <br></br> semillas secas, buen snap, buen brillo, sedoso, amargo ligero tostado. <br></br> <br></br>{" "}
+                <h6>Fluidez</h6> 5/5
+              </div>
+            </div>
           </div>
         </div>
       </div>
